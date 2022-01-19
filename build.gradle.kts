ea0d1980a136a00402402c90550436eaa5b702e8
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2022-01-19 09:43:18 +0100

    Created skeleton of desktop application

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.31"
+    id("org.jetbrains.compose") version "1.0.0"
+}
+
+description = "jactor::desktop"
+group = "com.github.jactor"
+version = "0.0.1"
+
+repositories {
+    google()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation(compose.desktop.currentOs)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "16"
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "jactor-desktop"
+            packageVersion = "1.0.0"
+        }
+    }
+}
